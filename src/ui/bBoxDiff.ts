96a4515fca3d01c5a31333d07b2c61538da8dd00
Lucas Crane <lucrane@pm.me>
2022-12-05 12:50:30 -0800

    fix typo

--- src/ui/bBoxDiff.ts
+++ src/ui/bBoxDiff.ts
@@ -3,5 +3,5 @@ import { numToPx } from '../util'
 export default function bBoxDiff (oldBBox: DOMRect, newBBox: DOMRect) {
   const x = oldBBox.x - newBBox.x
   const y = oldBBox.y - newBBox.y
-  return `translate(${numToPx(x)}}, ${numToPx(y)})`
+  return `translate(${numToPx(x)}, ${numToPx(y)})`
 }
