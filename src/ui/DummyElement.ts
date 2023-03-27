44373ae0ccb39df1cb38fb611f08ca74991b8362
Lucas Crane <lucrane@pm.me>
2023-03-26 17:48:39 -0700

    DummyElement: rename this.element to this.dummy

--- src/ui/DummyElement.ts
+++ src/ui/DummyElement.ts
@@ -3,106 +3,106 @@ import animateManual from './animateManual'
 import { makeStyle } from './makeStyle'
 
 export default class DummyElement {
-  element = document.createElement('div')
+  dummy = document.createElement('div')
 
-  style = getComputedStyle(this.element)
+  style = getComputedStyle(this.dummy)
 
   constructor (public original: HTMLElement) {
-    this.element.classList.add(dummyStyle)
+    this.dummy.classList.add(dummyStyle)
 
-    original.replaceWith(this.element)
-    this.element.append(original)
+    original.replaceWith(this.dummy)
+    this.dummy.append(original)
   }
 
   growSmart () {
-    const animation = this.element.animate({
+    const animation = this.dummy.animate({
       margin: [`0`, this.style.margin],
     }, options)
 
-    animateManual(this.element, (t) => {
-      this.element.style.width = `${t * this.original.offsetWidth}px`
-      this.element.style.height = `${t * this.original.offsetHeight}px`
+    animateManual(this.dummy, (t) => {
+      this.dummy.style.width = `${t * this.original.offsetWidth}px`
+      this.dummy.style.height = `${t * this.original.offsetHeight}px`
     }, options)
 
     animation.onfinish = () => {
-      this.element.replaceWith(this.original)
+      this.dummy.replaceWith(this.original)
     }
 
     return this
   }
 
   growWidthFirst () {
-    this.element.style.height = `0`
-    this.element.style.marginBlock = `0`
+    this.dummy.style.height = `0`
+    this.dummy.style.marginBlock = `0`
 
-    this.element.animate({
+    this.dummy.animate({
       width: [`0`, this.style.width],
       marginInline: [`0`, this.style.marginInline],
     }, options).onfinish = () => {
-      this.element.style.height = ``
-      this.element.style.marginBlock = ``
-      this.element.animate({
+      this.dummy.style.height = ``
+      this.dummy.style.marginBlock = ``
+      this.dummy.animate({
         height: [`0`, this.style.height],
         marginBlock: [`0`, this.style.marginBlock],
       }, options).onfinish = () => {
-        this.element.replaceWith(this.original)
+        this.dummy.replaceWith(this.original)
       }
     }
     return this
   }
 
   growWidthOnly () {
-    this.element.animate({
+    this.dummy.animate({
       width: [`0`, this.style.width],
       marginInline: [`0`, this.style.marginInline],
     }, options).onfinish = () => {
-      this.element.replaceWith(this.original)
+      this.dummy.replaceWith(this.original)
     }
     return this
   }
 
   growHeightOnly () {
-    this.element.animate({
+    this.dummy.animate({
       height: [`0`, this.style.height],
       marginBlock: [`0`, this.style.marginBlock],
     }, options).onfinish = () => {
-      this.element.replaceWith(this.original)
+      this.dummy.replaceWith(this.original)
     }
     return this
   }
 
   shrink () {
-    this.element.animate({
+    this.dummy.animate({
       height: [this.style.height, `0`],
       width: [this.style.width, `0`],
       margin: [this.style.margin, `0`],
     }, { ...options, duration: duration.slow }).onfinish = () => {
-      this.element.remove()
+      this.dummy.remove()
     }
     return this
   }
 
   shrinkHeightFirst () {
-    this.element.animate({
+    this.dummy.animate({
       height: [this.style.height, `0`],
       marginBlock: [this.style.marginBlock, `0`],
     }, options).onfinish = () => {
-      this.element.animate({
+      this.dummy.animate({
         width: [this.style.width, `0`],
         marginInline: [this.style.marginInline, `0`],
       }, options).onfinish = () => {
-        this.element.remove()
+        this.dummy.remove()
       }
     }
     return this
   }
 
   shrinkHeightOnly () {
-    this.element.animate({
+    this.dummy.animate({
       height: [this.style.height, `0`],
       marginBlock: [this.style.marginBlock, `0`],
     }, options).onfinish = () => {
-      this.element.remove()
+      this.dummy.remove()
     }
     return this
   }
