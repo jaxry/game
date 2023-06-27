66f86b5e68bfe786a8afb5680ff5d802024806ab
Lucas Crane <lucrane@pm.me>
2023-06-27 13:07:14 -0600

    simply drag and drop code

--- src/ui/DragAndDrop.ts
+++ src/ui/DragAndDrop.ts
@@ -1,6 +1,6 @@
 export default class DragAndDrop<T> {
-  callbacks = new Map<Element, Callbacks<T>>()
-
+  private callbacks = new Map<Element, Callbacks<T>>()
+  private dropEffect = new WeakMap<Element, DropEffect | false>()
   private payload: T | null = null
 
   drag (elem: HTMLElement, payload: T, image?: HTMLElement) {
@@ -13,9 +13,11 @@ export default class DragAndDrop<T> {
         const y = image.offsetHeight / 2 * window.devicePixelRatio
         e.dataTransfer!.setDragImage(image, x, y)
       }
+
       this.payload = payload
-      for (const { isDroppable } of this.callbacks.values()) {
-        isDroppable(payload)
+
+      for (const [element, { isDroppable }] of this.callbacks) {
+        this.dropEffect.set(element, isDroppable(payload))
       }
     })
 
@@ -33,27 +35,10 @@ export default class DragAndDrop<T> {
   }
 
   drop (element: HTMLElement, callbacks: Callbacks<T>) {
-    const { isDroppable, onDrop } = callbacks
-
     this.callbacks.set(element, callbacks)
 
-    let dropEffect: DropEffect | false = false
-
-    element.addEventListener('dragenter', (e) => {
-      // prevent unnecessary calls to this method
-      if (e.target !== e.currentTarget) {
-        return
-      }
-
-      dropEffect = isDroppable(this.payload!)
-
-      if (dropEffect) {
-        e.stopPropagation()
-        e.preventDefault()
-      }
-    })
-
     element.addEventListener('dragover', (e) => {
+      const dropEffect = this.dropEffect.get(element)
       if (dropEffect) {
         e.stopPropagation()
         e.preventDefault()
@@ -64,9 +49,7 @@ export default class DragAndDrop<T> {
     element.addEventListener('drop', (e) => {
       e.stopPropagation()
       e.preventDefault()
-      if (isDroppable(this.payload!)) {
-        onDrop(this.payload!)
-      }
+      callbacks.onDrop(this.payload!)
     })
 
     return () => {
